d129792ce4d75a87739459c312506f722b976ead
Hassan Abdul Rehman <hassan.rehman@gmail.com>
2022-09-10 16:18:09 +0100

    Proper parser

--- app/parser.rb
+++ app/parser.rb
@@ -1,32 +1,42 @@
 class Parser
   def initialize(arguments)
-    @input_file = arguments[0]
+    @file_reader = FileReader.new(arguments)
   end
 
   def run
     puts ordered_paths.map { |p| p.join(" ") }
   end
 
-  # Reads the @input_file and returns an ordered array with:
-  #   ["path", count]
-  #
   def visits
-    return [] if @input_file.nil? || !File.exist?(@input_file)
+    populate!
 
-    File.readlines(@input_file)
-      .select { |l| l.start_with?("/") }
-      .map { |l| l.split(" ") } # separate each path
-      .group_by(&:first).transform_values!(&:count) # Get the hash with counts
-      .to_a.sort_by { |k, v| [-1*v, k] } # Largest first
+    @content.transform_values { |visitors| visitors.values.sum }
+      .to_a
+      .sort_by { |path, count| [-1 * count, path] }
   end
 
   def unique_views
-    return [] if @input_file.nil? || !File.exist?(@input_file)
+    populate!
 
-    File.readlines(@input_file)
-      .select { |l| l.start_with?("/") }
-      .group_by { |l| l }
-      .transform_values!(&:count) # Get the hash with counts
-      .to_a.sort_by { |k, v| [-1*v, k] } # Largest first
+    @content.transform_values(&:count)
+      .to_a
+      .sort_by { |path, count| [-1 * count, path] }
+  end
+
+  private
+
+  # Reads the @input_file and populates the structure:
+  #   { path: { visitor: count_of_views } }
+  #
+  def populate!
+    return @content if @populated
+
+    @populated = true
+    @content = Hash.new { |h, k| h[k] = Hash.new { |h1, k1| h1[k1] = 0 } }
+
+    @file_reader.call do |page, ip|
+      @content[page][ip] += 1
+    end
+    @content
   end
 end
